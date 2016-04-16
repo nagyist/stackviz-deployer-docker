3be9e4cb5dbb2268b9f73bb174b30e8ac196c8ea
Tim Buckley <timothyb89@gmail.com>
2016-04-15 18:23:48 -0600

    Enable uwsgi logging and provide better status updates in the build script.

--- build.sh
+++ build.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
+set -e
+set -x
 
 if [ -d stackviz-deployer ]; then
-    pushd stackviz-deployer
-    git pull origin master
-    popd
+    echo "--> Using existing copy of stackviz-deployer"
 else
+    echo "--> Checking out fresh copy of stackviz-deployer"
     git clone https://github.com/timothyb89/stackviz-deployer
 fi
 
 if [ -d stackviz ]; then
-    pushd stackviz
-    git pull origin master
-    popd
+    echo "--> Using existing copy of stackviz"
 else
+    echo "--> Checking out fresh copy of stackviz"
     git clone https://github.com/openstack/stackviz
     pushd stackviz
     npm install
     popd
 fi
 
+echo "--> Running 'gulp build' on stackviz"
 pushd stackviz
 gulp prod
 popd
+
+echo "--> Building docker image: $1"
 
 sudo docker build -t $1 .
